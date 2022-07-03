390961addc16155405565bbb38af372e83128a76
Farouk Ech-charef <fech-cha@e1r5p12.1337.ma>
2022-07-04 00:37:12 +0100

    learning cpp

--- learn_cpp/first_I&O.cpp
+++ learn_cpp/first_I&O.cpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first.cpp                                          :+:      :+:    :+:   */
+/*   first_I&O.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 07:10:23 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/07/01 04:04:37 by fech-cha         ###   ########.fr       */
+/*   Updated: 2022/07/03 18:36:40 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //Input and output
-// the prefix std:: indicates that the names cout and cin are defined inside the namespace named std;
+// the prefix std:: indicates that the full_names cout and cin are defined inside the full_namespace full_named std;
 // :: is the scope operator
 
 #include <iostream>
@@ -34,6 +34,20 @@
 
 int main ()
 {
-	std::cout << "Testing newline. \n";
-	std::cout << "Test2";
+	// std::cout << "Testing newline. \n";
+	// std::cout << "Test2";
+	//get Data with spaces
+	int	num;
+	
+	std::string full_name;
+
+	std::cout << "Enter full_Name :" << std::endl;
+	std::getline(std::cin, full_name);
+	
+	std::cout << "Enter Age :" << std::endl;
+	std::cin >> num;
+
+	std::cout << "full_Name : " << full_name << std::endl << "Age : " << num << std::endl;
+	
+	return (0);
 }
